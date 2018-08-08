8df45b13c5f26486c852a4c2e0f2faa336690345
Lucas Clarke <lucaspatrickclarke@gmail.com>
2018-08-08 19:10:54 -0400

    First Push
    -

---
+++ html/laboratory/laboratory.php
@@ -0,0 +1,52 @@
+<html>
+    
+<head>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+    
+<body>
+    
+    <div>
+        <h1>Gile Chemical Document System and Intranet Site</h1>
+        
+    </div>
+    
+    <table>
+        <tr>
+            <th>Administration</th>
+            <th>Facilities</th>
+            <th>Service</th>
+        </tr>
+        <tr>
+            <td>Customer Service</td>
+            <td>Manufacturing</td>
+            <td>Maintenance</td>
+        </tr>
+        <tr>
+            <td>IT Support</td>
+            <td>Repackaging</td>
+            <td>Engineering</td>
+        </tr>
+        <tr>
+            <td>Supply Chain</td>
+            <td>Greendale</td>
+            <td>Quality Assurance</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>CPG</td>
+            <td>Laboratoy</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>Safety</td>
+        </tr>
+    
+    </table>
+    
+    
+    
+</body>
+
+</html>
