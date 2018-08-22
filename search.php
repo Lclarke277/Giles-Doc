36d09899b191cd457d69f4a862b45e6c387a1dca
Lucas Clarke <lucaspatrickclarke@gmail.com>
2018-08-22 10:31:48 -0400

    Mobile CSS except index.php

--- search.php
+++ search.php
@@ -1,7 +1,8 @@
 <html>
     
 <head>
-<link rel='stylesheet' type='text/css' href="./searchStylesheet.css">
+  <meta name="viewport" content="width=device-width,initial-scale=1">    
+  <link rel='stylesheet' type='text/css' href="./searchStylesheet.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
     
